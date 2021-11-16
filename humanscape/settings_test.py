f90b9640ff5d4fbe28aa8e0140aa70750e405425
qhsdnr0 <qhsdnr0@gmail.com>
2021-11-16 14:02:26 +0900

    Modify : Test Database
    - 테스트용 데이터베이스 생성 및 settings_test파일 경로 수정

--- humanscape/settings_test.py
+++ humanscape/settings_test.py
@@ -1,3 +1,10 @@
 from .settings import *
 
 TEST_RUNNER = 'humanscape.test_runner.TestRunner'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'test_db.sqlite3',
+    }
+}
