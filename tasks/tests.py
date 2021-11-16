acc5aa82db77e520142fac94fe4fd29f10453ef0
palza4dev <moonsj6940@gmail.com>
2021-11-16 15:44:44 +0900

    Modify: task search&list view 통합 - get 메소드로 검색과 목록 조회 기능 통합 - 하나의 url로 조건별 필터링 가능 - unittest 통과 완료

--- tasks/tests.py
+++ tasks/tests.py
@@ -11,8 +11,75 @@ class TasksTest(TestCase):
     def tearDown(self):
         pass
 
-    def test_search_test(self):
-        response = client.get("/tasks/search?title=한국인&department=cardiology")
+    def test_list_get_success(self):
+        response = client.get('/tasks?offset=0&limit=5', content_type="application/json")
+    
+        data = [
+        {
+            "number": "C130010",
+            "title": "조직구증식증 임상연구 네트워크 구축 및 운영(HLH)",
+            "department": "Pediatrics",
+            "institute": "서울아산병원",
+            "number_of_target": "120",
+            "duration": "3년",
+            "type": "관찰연구",
+            "trial_stage": "코호트",
+            "scope": "국내다기관"
+        },
+        {
+            "number": "C130011",
+            "title": "대한민국 쇼그렌 증후군 코호트 구축",
+            "department": "Rheumatology",
+            "institute": "가톨릭대 서울성모병원",
+            "number_of_target": "500",
+            "duration": "6년",
+            "type": "관찰연구",
+            "trial_stage": "코호트",
+            "scope": "국내다기관"
+        },
+        {
+            "number": "C100002",
+            "title": "Lymphoma Study for Auto-PBSCT",
+            "department": "Hematology",
+            "institute": "가톨릭대 서울성모병원",
+            "number_of_target": "",
+            "duration": "1년",
+            "type": "관찰연구",
+            "trial_stage": "코호트",
+            "scope": "단일기관"
+        },
+        {
+            "number": "C110007",
+            "title": "악성림프종의 임상양상과 항암 화학요법의 치료 성적 조사 및 예후 예측 지표 분석, retrospective study",
+            "department": "Hematology",
+            "institute": "가톨릭대 서울성모병원",
+            "number_of_target": "200",
+            "duration": "",
+            "type": "관찰연구",
+            "trial_stage": "Case-only",
+            "scope": "단일기관"
+        },
+        {
+            "number": "C140012",
+            "title": "우울증 임상연구네트워크구축",
+            "department": "Psychiatry",
+            "institute": "가톨릭대 여의도성모병원",
+            "number_of_target": "300",
+            "duration": "11개월",
+            "type": "관찰연구",
+            "trial_stage": "코호트",
+            "scope": "국내다기관"
+        }]
+        
+        result = {
+            'count' : 5,
+            'data'  : data
+        }    
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), result)
+        
+    def test_search_get_value_error_fail(self):
+        response = client.get("/tasks?title=한국인&department=cardiology")
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), 
         {
@@ -44,11 +111,11 @@ class TasksTest(TestCase):
             })
         
         # 알맞지 않은 값을 보낸 경우 400을 리턴합니다.
-        response = client.get("/tasks/search?limit=xxx")
+        response = client.get("/tasks?limit=xxx")
         self.assertEqual(response.status_code,400)
-        self.assertEqual(response.json(), {'messsage' : 'TYPE_ERROR'})
+        self.assertEqual(response.json(), {'messsage' : 'VALUE_ERROR'})
         
-    def test_a_detail_get_success(self):
+    def test_detail_get_success(self):
         
         response = client.get('/tasks/5')
         created_at = Task.objects.get(id=5).created_at
@@ -71,77 +138,9 @@ class TasksTest(TestCase):
             }
         })
         
-    def test_a_detail_get_task_does_not_exist(self):
+    def test_detail_get_task_does_not_exist(self):
         
         client = Client()
         response = client.get('/tasks/277')
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.json(),{
-            'message' : 'TASK_DOES_NOT_EXIST',
-            
-        })   
-
-    def test_list_get_success(self):
-
-        data = [
-        {
-            "title": "조직구증식증 임상연구 네트워크 구축 및 운영(HLH)",
-            "number": "C130010",
-            "duration": "3년",
-            "number_of_target": "120",
-            "trial_stage": "코호트",
-            "department": "Pediatrics",
-            "institute": "서울아산병원",
-            "scope": "국내다기관",
-            "type": "관찰연구"
-        },
-        {
-            "title": "대한민국 쇼그렌 증후군 코호트 구축",
-            "number": "C130011",
-            "duration": "6년",
-            "number_of_target": "500",
-            "trial_stage": "코호트",
-            "department": "Rheumatology",
-            "institute": "가톨릭대 서울성모병원",
-            "scope": "국내다기관",
-            "type": "관찰연구"
-        },
-        {
-            "title": "Lymphoma Study for Auto-PBSCT",
-            "number": "C100002",
-            "duration": "1년",
-            "number_of_target": "",
-            "trial_stage": "코호트",
-            "department": "Hematology",
-            "institute": "가톨릭대 서울성모병원",
-            "scope": "단일기관",
-            "type": "관찰연구"
-        },
-        {
-            "title": "악성림프종의 임상양상과 항암 화학요법의 치료 성적 조사 및 예후 예측 지표 분석, retrospective study",
-            "number": "C110007",
-            "duration": "",
-            "number_of_target": "200",
-            "trial_stage": "Case-only",
-            "department": "Hematology",
-            "institute": "가톨릭대 서울성모병원",
-            "scope": "단일기관",
-            "type": "관찰연구"
-        },
-        {
-            "title": "우울증 임상연구네트워크구축",
-            "number": "C140012",
-            "duration": "11개월",
-            "number_of_target": "300",
-            "trial_stage": "코호트",
-            "department": "Psychiatry",
-            "institute": "가톨릭대 여의도성모병원",
-            "scope": "국내다기관",
-            "type": "관찰연구"
-        }
-    ]
-
-        response = client.get('/tasks/list?offset=0&limit=5', content_type="application/json")
-
-        self.assertEqual(response.json(), {'data' : data})
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(),{'message' : 'TASK_DOES_NOT_EXIST'})
--- tasks/views.py
+++ tasks/views.py
@@ -1,19 +1,16 @@
-from datetime     import datetime, timedelta
+from datetime         import datetime, timedelta
 
-from json.decoder import JSONDecodeError
+from django.db.models import Q
+from django.views     import View 
+from django.http      import JsonResponse
 
-from django.db.models       import Q
-from django.views           import View 
-from django.http            import JsonResponse
-from django.core.exceptions import ValidationError
+from .models          import Task
 
-from .models          import *
-
-class SearchView(View):
+class TaskListView(View):
     def get(self, request):
         try:
-            offset = request.GET.get('offset', 0)
-            limit  = request.GET.get('limit', 30)
+            offset = int(request.GET.get('offset', 0))
+            limit  = int(request.GET.get('limit', 10))
 
             title       = request.GET.get('title', None)
             department  = request.GET.get('department', None)
@@ -41,10 +38,15 @@ class SearchView(View):
             
             if scope:
                 q.add(Q(scope__name=scope), q.AND)
+            
+            now = datetime.now()
 
-            tasks = Task.objects.filter(q)\
-                                .select_related('department', 'institute', 'type', 'trial_stage', 'scope')\
-                                .order_by('updated_at')[offset:offset+limit]
+            tasks = Task.objects.select_related(
+                'department',
+                'institute',
+                'type',
+                'trial_stage',
+                'scope').filter(q, updated_at__range=[now - timedelta(days=7), now]).order_by('updated_at')[offset:offset+limit]
 
             result = {
                 'count' : len(tasks),
@@ -65,6 +67,9 @@ class SearchView(View):
         
         except TypeError:
             return JsonResponse({'messsage' : 'TYPE_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'messsage' : 'VALUE_ERROR'}, status=400)
+            
         
 class TaskDetailView(View):
     def get(self, request, task_id):
@@ -95,41 +100,3 @@ class TaskDetailView(View):
         
         except Task.DoesNotExist:
             return JsonResponse({'message' : 'TASK_DOES_NOT_EXIST'}, status = 400)
-
-class TaskList(View):
-  def get(self, request):
-    try:
-      offset = int(request.GET.get('offset', 0))
-      limit  = int(request.GET.get('limit', 10))
-      now    = datetime.now()
-
-      one_week_list = Task.objects.select_related(
-        'trial_stage', 
-        'department', 
-        'institute', 
-        'scope',
-        'type'
-        ).filter(updated_at__range=[now - timedelta(days=7), now])[offset:offset+limit]
-      
-      data = [{
-        'title' : value.title,
-        'number' : value.number,
-        'duration' : value.duration,
-        'number_of_target' : value.number_of_target,
-        'trial_stage' : value.trial_stage.name,
-        'department' : value.department.name,
-        'institute' : value.institute.name,
-        'scope' : value.scope.name,
-        'type' : value.type.name
-      } for value in one_week_list]
-
-      return JsonResponse({'data' : data }, status=201)
-
-    except KeyError :
-      return JsonResponse({'message' : 'KEY_ERROR'}, status=400)
-
-    except JSONDecodeError:
-      return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status = 400)
-      
-    except ValidationError as e:
-      return JsonResponse({'message' : (e.message)}, status = 400)
