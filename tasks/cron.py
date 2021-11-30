be2fce83dc109d496e10cf0c63dad8d313d879b7
thisisempty <86424094+thisisempty@users.noreply.github.com>
2021-12-01 00:05:17 +0900

    Revert "[Refactor] use count, update_or_create method"

--- tasks/cron.py
+++ tasks/cron.py
@@ -17,30 +17,36 @@ def daily_batch_task():
     'perPage'    : get_total_count(),
     'serviceKey' : OPEN_API_SERVICE_KEY
     }
-    
     response = requests.get(OPEN_API_URL, params=params).json()
     
     for task in response['data']:
-        title                = task['과제명'],
-        duration             = task['연구기간'],
-        number_of_target     = task['전체목표연구대상자수'],
         department, created  = Department.objects.get_or_create(name=task['진료과']) if task['진료과'] else (None, False)
         institute, created   = Institute.objects.get_or_create(name=task['연구책임기관']) if task['연구책임기관'] else (None, False)
         type, created        = Type.objects.get_or_create(name=task['연구종류']) if task['연구종류'] else (None, False)
         trial_stage, created = TrialStage.objects.get_or_create(name=task['임상시험단계(연구모형)']) if task['임상시험단계(연구모형)'] else (None, False)
         scope, created       = Scope.objects.get_or_create(name=task['연구범위']) if task['연구범위'] else (None, False)
 
-        Task.objects.update_or_create(
-            number   = task['과제번호'],
-            defaults = {
-                    title            : title,
-                    duration         : duration,
-                    number_of_target : number_of_target,
-                    department       : department,
-                    institute        : institute,
-                    type             : type,
-                    trial_stage      : trial_stage,
-                    scope            : scope
-                }
+        if Task.objects.filter(number=task['과제번호']).exists():
+            Task.objects.filter(number=task['과제번호']).update(
+                number           = task['과제번호'],
+                title            = task['과제명'],
+                duration         = task['연구기간'],
+                number_of_target = task['전체목표연구대상자수'],
+                department       = department,
+                institute        = institute,
+                type             = type,
+                trial_stage      = trial_stage,
+                scope            = scope
             )
-
+        else:
+            Task.objects.create(
+                number           = task['과제번호'],
+                title            = task['과제명'],
+                duration         = task['연구기간'],
+                number_of_target = task['전체목표연구대상자수'],
+                department       = department,
+                institute        = institute,
+                type             = type,
+                trial_stage      = trial_stage,
+                scope            = scope
+            )
